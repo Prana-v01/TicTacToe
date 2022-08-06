4a984c2ae3b49666195c2596f83aa91ba768c004
Pranav <jpranav077@gmail.com>
2022-08-05 20:40:41 -0500

    Update tic_tac_toe_code.py
    Added win conditions to the game so it can determine who wins. No tie implemented yet.
    bugs:
     -> If i enter 9, it automatically ends game.
    -> if i enter 8,9,7 it also automatically ends game.

--- tic_tac_toe_code.py
+++ tic_tac_toe_code.py
@@ -1,61 +1,53 @@
 board = ['▢','▢','▢','▢','▢','▢','▢','▢','▢']
 
-def print_board():
+def print_board(): # board will be displayed
     print(board[0],board[1],board[2])
     print(board[3], board[4], board[5])
     print(board[6], board[7], board[8])
 
-print_board()
+print_board() #displaying board
 
-def validate(board,player): #validates user's choice
-    while player not in range(1,10) or board[player-1] != '▢': #loop will only be triggered if user's responce is invalid.
-        player = int(input("Please enter a valid position: ")) #asks for new number
-    return player-1 #returns the correct number.
+def validate(board,player): #to validate the users input of position on board.
+    while player not in range(1,10) or board[player-1] != '▢': #checks if player is not in valid range or if theres already slot being used. If either is True, contiue.
+        player = int(input("Please enter a valid position: ")) # asks for new position, as the previous one was incorrect.
+    return player-1 #It returns -1 so we recieve the index and is able to place in the correct place.
 
-def game_conditions(board): #win conditions -> not finished. Implementing: Tie, Player 2's win (else)
-    if board[0] == "x" and board[1] == "x" and board[2] == "x":
-        print("player 1 won!")
-    elif board[3] == "x" and board[4] == "x" and board[5] == "x":
-        print("player 1 has won!!")
-    elif board[6] == "x" and board[7] == "x" and board[8] == "x":
-        print("player 1 has won")
-        ### horizonal player 1 win
-    elif board[0] == "x" and board[4] == "x" and board[8] == "x":
-        print("player 1 has won")
-    elif board[2] == "x" and board[4] == "x" and board[6] == "x":
-        print("player 1 has won!")
-        ### Diagnoal player 1 win
-    elif board[0] == "x" and board[3] == "x" and board[6] == "x":
-        print("player 1 has won")
-    elif board[1] == "x" and board[4] == "x" and board[7] == "x":
-        print("player 1 has won")
-    elif board[2] == "x" and board[5] == "x" and board[8] == "x":
-        print("player 1 has won")
-        ### vertical player 1
-    else:
-        print("player two won!")
-        # if none of them are True, player 2 must have won. Or a tie...
+def win_conditions(player_value,player_name): #this will determnie who wins.
+    win_conditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]] # the possible win combinations.
+    for wins in win_conditions: #each list in the list of lists.
+        occurence_of_combination = 0 #keeps track of how many time the occurence of a correct combination occures.
+        for num in player_value: #the single element inside the original list of users input
+            if num in wins: #checks if that single element is iniside of a list inside the list of lists.
+                occurence_of_combination += 1 # if yes, it adds +1 
+                if occurence_of_combination >= 3: # once the user has 3 correct occurences, it translates to a win. 
+                    return True
 
-while True: #this is the gameplay
+#these are to store the input given by the two users.
+p1_value = []
+p2_value = []
+
+#this is the game. (Could also make this a function).
+while True:
     player_one = int(input("enter a number: "))
-    player_one_valid = validate(board,player_one) #calls validate and stores it in player_one_valid, because its the valid responce hence valid.
-    board[player_one_valid] = "x" #sets "x" in that position the user desired-1 
-    print_board()
+    player_one_valid = validate(board,player_one) #validates the users input, then stores, so we can further use.
+    p1_value.append(player_one_valid) #the validated version of the users input will then be appended to p1_value.
+    board[player_one_valid] = "x" # places "x" on the board of the desired place by the user.
+    print_board() #displays board
 
+    # checks if game is finished by determining the amount of slots. When no slots are availbile the game will end.
+    # this is flawed in a way, this should end when somone wins. Or fill up the slots if its a tie. 
     if board[0] and board[1] and board[2] and board[3] and board[4] and board[5] and board[6] and board[7] and board[8] != "▢":
-        break #checks if the board is full.
-        #i wouldnt need this if i complete the conditions and i can also implement the gameplay as a function.
+        break
 
     player_two = int(input("enter a number: "))
     player_two_valid = validate(board, player_two)
+    p2_value.append(player_two_valid)
     board[player_two_valid] = "o"
     print_board()
 
+if win_conditions(p1_value,"Player One"):
+    print("Player one has won!") # display who wins.
 
-
-print()
-game_conditions(board) #determines who won.
-
-
-
+if win_conditions(p2_value,"Player Two"):
+    print("player two has won!")
 
