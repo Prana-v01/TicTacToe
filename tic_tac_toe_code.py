ea46b9d7535ea657dbcccf510d6ad65eeddb3dd1
Pranav <jpranav077@gmail.com>
2022-08-06 10:52:48 -0500

    Update tic_tac_toe_code.py
    +Added function descriptions

--- tic_tac_toe_code.py
+++ tic_tac_toe_code.py
@@ -1,6 +1,11 @@
-board = ['▢','▢','▢','▢','▢','▢','▢','▢','▢']
+board = ['▢','▢','▢','▢','▢','▢','▢','▢','▢'] #the format of the board.
 
 def print_board(): # board will be displayed
+    """
+    Purpose: This prints a nice version of the board, in the traditional way tic tac toe is played.
+    Parameters: None
+    Return: print
+    """
     print(board[0],board[1],board[2])
     print(board[3], board[4], board[5])
     print(board[6], board[7], board[8])
@@ -8,11 +13,25 @@ def print_board(): # board will be displayed
 print_board() #displaying board
 
 def validate(board,player): #to validate the users input of position on board.
+    """
+    Purpose: This function validates the users input. It takes in the orginal value and checks to see if it meets the critera set.
+    If the criteria is not met for any reason, it will give them an option to try another input.
+    Parameters:
+    1) board: The board is here so we can check one of the conditions, if the specific slot if allready being used.
+    2) player: This is to recieve the users input.
+    Return: player-1, which gives the index and is usable in relation to the board.
+    """
     while player not in range(1,10) or board[player-1] != '▢': #checks if player is not in valid range or if theres already slot being used. If either is True, contiue.
         player = int(input("Please enter a valid position: ")) # asks for new position, as the previous one was incorrect.
     return player-1 #It returns -1 so we recieve the index and is able to place in the correct place.
 
 def win_conditions(player_value): #this will determnie who wins.
+    """
+    Purpose: This function determines who wins the match
+    Parameters: 
+    1) player_value: This takes in the player's value (their valid inputs) and try to match them with one of the winning combinations)
+    Return: True
+    """
     win_conditions = [[0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]] # the possible win combinations.
     for wins in win_conditions: #each list in the list of lists.
         occurence_of_combination = 0 #keeps track of how many time the occurence of a correct combination occures.
